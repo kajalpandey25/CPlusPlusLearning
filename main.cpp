9e5a0665403383710d3f0255ba1032176c837a80
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2023-11-09 22:10:52 +0530

    Ok

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout<<"Hello World Kajal";
-    return 0;
-}
+int main()
+{
+    // Single line comments
 
+    /* Double line comments*/
+    // cout << "Hello World Kajal" << endl;
+    // cout << "Next Line";
+    //   return 0;
+
+    // Data type
+    // int a, b, c;
+
+    // Variables
+    // short _sa = 9;
+    // cout <<_sa;
+
+    // CamelCase Notation
+   /* int marksInMaths= 83;
+    cout<<"The marks of the students in maths is "<<marksInMaths;*/
+
+    // short a;
+    // int b;
+    // long c;
+    // long long d;
+    // float score = 45.32;
+    // cout<<"The score is "<<score;
+
+}
