c1051a2e6adaada540ccd34d34d4c52f276822c4
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2023-11-10 22:22:50 +0530

    Ok

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+// f(x) = x^2 + 2
+int add(int a, int b)
+{
+    int c;
+    c = a + b;
+    return c;
+}
+
 int main()
 {
     // Single line comments
@@ -18,8 +26,8 @@ int main()
     // cout <<_sa;
 
     // CamelCase Notation
-   /* int marksInMaths= 83;
-    cout<<"The marks of the students in maths is "<<marksInMaths;*/
+    /* int marksInMaths= 83;
+     cout<<"The marks of the students in maths is "<<marksInMaths;*/
 
     // short a;
     // int b;
@@ -28,4 +36,104 @@ int main()
     // float score = 45.32;
     // cout<<"The score is "<<score;
 
+    // int a, b;
+    // cout << "Enter First Number" << endl;
+    // cin >>a;
+
+    // cout << "Enter Second Number" << endl;
+    // cin >>b;
+
+    // cout<<"a + b is "<<a + b<<endl;
+    // cout<<"a - b is "<<a - b<<endl;
+    // cout<<"a * b is "<<a * b<<endl;
+    // cout<<"a / b is "<<(float) a / b<<endl;
+
+    // If- else Condition
+
+    // int age;
+    // cout << "Enter your age " << endl;
+    // cin >> age;
+
+    // if (age > 150)
+    // {
+    //     cout << "Invalid age";
+    // }
+    // else if (age >= 18)
+    // {
+    //     cout << "You can vote";
+    // }
+    // else
+    // {
+    //     cout << "You Cannot vote";
+    // }
+
+    // Switch Statement
+
+    // int age;
+    // cout << "Enter your age " << endl;
+    // cin >> age;
+    // switch (age)
+    // {
+    // case 12:
+    //     cout << "You are 12 years old";
+    //     break;
+    // case 18:
+    //     cout << "You are 18 years old";
+    //     break;
+
+    // default:
+    //     cout << "You are neither 12 nor 18 years old";
+    //     break;
+    // }
+
+    // Loops
+    // 1. While Loop
+    // int index = 0;
+    // while (index < 34)
+    // {
+    //     cout << "We are at index number " << index << endl;
+    //     index = index + 1;
+    // }
+
+    // 2. Do-While Loop
+    // int index = 0;
+    // do
+    // {
+    //     cout << "We are at index number " << index << endl;
+    //     index = index + 1;
+    // } while (index < 33);
+
+    // 3. For Loop
+    //  for (int i = 0; i < 34; i++)
+    //  {
+    //      cout << "The value of i is " << i << endl;
+    //  }
+
+    // Functions
+
+    // int a, b;
+    // cout << "Enter First Number" << endl;
+    // cin >> a;
+
+    // cout << "Enter Second Number" << endl;
+    // cin >> b;
+    // cout << "The function returned " << add(a, b);
+
+    // Arrays
+
+    // int arr[3] = {1, 3, 4};
+    // cout << arr[1];
+
+    // int marks[6];
+    // for (int i = 0; i < 6; i++)
+    // {
+    //     cout << "Enter the marks of " << i << "th student" << endl;
+    //     cin >> marks[i];
+    // }
+    // for (int i = 0; i < 6; i++)
+    // {
+    //     cout << "Marks of " << i << "th student is " << marks[i] << endl;
+    // }
+
+    
 }
